79abe1b6046ca18be45dde7da775098e9035a9ec
Logan Butler <logan3528@gmail.com>
2022-04-26 12:00:19 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace solo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name? ");
+            String fName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            String lName = Console.ReadLine();
+            
+            Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
+        }
+    }
+}
